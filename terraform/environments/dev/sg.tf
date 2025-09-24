e40f5231bba9b5dc1c1e36afbc464da2b04fa94f
gtsintsadze <giotsintsadze94@gmail.com>
2025-09-25 02:43:18 +0400

    ts

--- terraform/environments/dev/sg.tf
+++ terraform/environments/dev/sg.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "internet" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -19,13 +26,4 @@ resource "aws_security_group" "internet" {
   tags = {
     Name = "internet-sg"
   }
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.internet.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 22
-  to_port           = 22
 }
