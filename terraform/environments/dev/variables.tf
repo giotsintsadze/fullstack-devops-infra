08b7f104ed2abb8eafc09687126b632fdf9098e5
gtsintsadze <giotsintsadze94@gmail.com>
2025-09-25 01:08:25 +0400

    Add Ansible playbook stage to Jenkins pipeline for app deployment

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -3,3 +3,15 @@ variable "base_region" {
   default     = "eu-central-1"
   description = "AWS Region"
 }
+
+variable "key_name" {
+  type        = string
+  description = "secure key name"
+  default     = "nat"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "default instnace type for testing"
+}
